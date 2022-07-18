5bce76ed9345517fc3cd78332900d27aabd2b6cb
azoulay <d.e.azoulay@gmail.com>
2022-07-18 20:41:47 +0300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+
+const server = Express();
+server.get('/', (req, res) => res.send('ok'));
+server.get('/_ah/health', (req, res) => res.send('ok'));
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.setGlobalPrefix('api');
   await app.listen(8000);
 }
 bootstrap();
